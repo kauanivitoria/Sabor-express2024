f6063d2e6f367b795bb593a133aa03fed202427d
kauanivitoria <140725971+kauanivitoria@users.noreply.github.com>
2024-08-27 11:27:18 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-print("""
-      Vitória Modas\n
-      
+import os
+
+def mostra_titulo():
+    print("""
       
 ██╗░░░██╗██╗████████╗░█████╗░██████╗░██╗░█████╗░  ███╗░░░███╗░█████╗░██████╗░░█████╗░░██████╗
 ██║░░░██║██║╚══██╔══╝██╔══██╗██╔══██╗██║██╔══██╗  ████╗░████║██╔══██╗██╔══██╗██╔══██╗██╔════╝
@@ -8,13 +9,46 @@ print("""
 ░╚████╔╝░██║░░░██║░░░██║░░██║██╔══██╗██║██╔══██║  ██║╚██╔╝██║██║░░██║██║░░██║██╔══██║░╚═══██╗
 ░░╚██╔╝░░██║░░░██║░░░╚█████╔╝██║░░██║██║██║░░██║  ██║░╚═╝░██║╚█████╔╝██████╔╝██║░░██║██████╔╝
 ░░░╚═╝░░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚═╝  ╚═╝░░░░░╚═╝░╚════╝░╚═════╝░╚═╝░░╚═╝╚═════╝░
-      
       """)
 
-print("1. Cadastrar Roupas")
-print("2. Listar roupa")
-print("3. Verificar estoque")
-print("4. Sair")
+def mostra_escolhas():
+    print("1. Cadastrar Roupas")
+    print("2. Listar Roupas")
+    print("3. Ativar Estoque ")
+    print("4. Sair")
 
-opcao_escolhida = input("Escolhe uma opção:")
-print("Voce escolheu uma opção:", opcao_escolhida )
+def mostra_opcao():
+
+    def finalizar_programa():
+        os.system("cls") 
+        print("Finalizando o programa\n") 
+
+    def opcao_invalida():
+        print("Esse caracter não é permitido")
+        input("Aperte qualquer tecla para voltar")
+        main()  
+
+    try:
+
+        opcao_escolhida = int(input("Escolha uma opção: "))
+
+        if opcao_escolhida == 1:
+            print("Você escolheu Cadastrar Medicamentos")
+        elif opcao_escolhida == 2:
+            print("Você escolheu Listar Medicamentos")
+        elif opcao_escolhida == 3:
+            print("Você escolheu Ativar Estoque")
+        elif opcao_escolhida == 4:
+            finalizar_programa()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+
+def main():
+    mostra_titulo()
+    mostra_escolhas()
+    mostra_opcao()
+
+if __name__ == "__main__":
+    main()
