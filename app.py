40c5f2b539df38cf507be58be74e88c4fa87869f
kauanivitoria <140725971+kauanivitoria@users.noreply.github.com>
2024-09-03 11:13:44 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def mostra_titulo():
     """)
 
 def mostra_escolhas():
-    print("1. Cadastrar Roupas")
+    print("1. Cadastrar Roupa")
     print("2. Listar Roupas")
     print("3. Ativar Estoque")
     print("4. Sair")
@@ -26,76 +26,73 @@ def mostra_escolhas():
 def escolhe_opcao():
     def exibir_subtitulo(texto):
         os.system("cls" if os.name == "nt" else "clear")
+        linha = "_" * 65
+        print(linha)
         print(texto)
+        print(linha)
         print(" ")
-      
+
     def retorna_menu():
         input("Digite uma tecla para voltar ao menu principal ")
         main()
-    
-    def cadastrar_roupas():
-        exibir_subtitulo("Cadastrar Roupa")
 
-        marca_roupas = input("Digite a marca da roupa que deseja cadastrar: ")
-        categoria_roupas = input("Digite a categoria que deseja cadastrar: ")
-        dados_da_roupa = {"Marca": marca_roupas, "categoria": categoria_roupas, "ativo": True}
-        Roupas.append(dados_da_roupa)
+    def cadastra_Roupa():
+        exibir_subtitulo("Cadastrar Roupas")
+        marca_Roupa = input("Digite o nome da marca que deseja cadastrar: ")
+        categoria_Roupa = input(f"Digite a categoria da roupa {marca_Roupa}: ")
+        dados_da_Roupa = {"Marca": marca_Roupa, "categoria": categoria_Roupa, "ativo": True}
+        Roupas.append(dados_da_Roupa)
+        print(f"A roupa {marca_Roupa} foi cadastrada com sucesso\n")
         retorna_menu()
-    
-    def listar_roupas():
+
+    def listar_Roupas():
         exibir_subtitulo("Lista de Roupas Cadastradas")
-        if not Roupas:
-            print("Nenhuma roupa cadastrada.")
-        else:
-            for roupa in Roupas:
-                marca = roupa["Marca"]
-                categoria = roupa["categoria"]
-                ativo = "Sim" if roupa["ativo"] else "Não"
-                print(f" - {marca} | {categoria} | {ativo}")
+        for roupa in Roupas:
+            marca_Roupa = roupa["Marca"]
+            categoria_Roupa = roupa["categoria"]
+            ativo = "Com estoque" if roupa["ativo"] else "Sem estoque"
+            print(f" - {marca_Roupa.ljust(20)} | {categoria_Roupa.ljust(20)} | {ativo}")
         retorna_menu()
-    
-    def ativar_estoque():
+
+    def ativar_Estoque():
         exibir_subtitulo("Ativar Estoque")
-        marca_roupas = input("Digite o nome da marca que deseja ativar: ")
-        marca_encontrada = False
+        marca_Roupa = input("Digite o nome da roupa que deseja ativar: ")
+        roupa_encontrada = False
 
         for roupa in Roupas:
-            if roupa["Marca"] == marca_roupas:
-                marca_encontrada = True
-                roupa["ativo"] = not cliente ["ativo"]
-                mensagem = f"o cadastro do {Roupas} foi ativado/desativado com sucesso" if
-
-        if not marca_encontrada:
-            print(f"Marca {marca_roupas} não encontrada.")
+            if marca_Roupa == roupa["Marca"]:
+                roupa_encontrada = True
+                roupa["ativo"] = not roupa["ativo"]
+                mensagem = f"{marca_Roupa} foi ativado com sucesso" if roupa["ativo"] else f"A roupa {marca_Roupa} foi desativada"
+                print(mensagem)
+                break
+        if not roupa_encontrada:
+            print("Roupa não encontrada")
         retorna_menu()
-    
+
     def finalizar_programa():
-        os.system("cls" if os.name == "nt" else "clear") 
-        print("Finalizando o programa\n") 
-        exit()
-    
+        os.system("cls" if os.name == "nt" else "clear")
+        print("Finalizando o programa\n")
+
     def opcao_invalida():
-        print("Opção inválida. Tente novamente.")
+        print("Essa opção não é válida")
         input("Aperte qualquer tecla para voltar")
-        main()  
-    
+        main()
+
     try:
         opcao_escolhida = int(input("Escolha uma opção: "))
-
         if opcao_escolhida == 1:
-            cadastrar_roupas()
+            cadastra_Roupa()
         elif opcao_escolhida == 2:
-            listar_roupas()
+            listar_Roupas()
         elif opcao_escolhida == 3:
-            ativar_estoque()
+            ativar_Estoque()
         elif opcao_escolhida == 4:
             finalizar_programa()
         else:
             opcao_invalida()
     except ValueError:
-        print("Entrada inválida. Por favor, insira um número.")
-        input("Aperte qualquer tecla para voltar")
-        main()
+        opcao_invalida()
 
 def main():
     mostra_titulo()
